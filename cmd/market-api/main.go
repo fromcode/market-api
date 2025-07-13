be730eec7963cd4bfcc93c1fb986e3d1a13ae5a1
fromcode <wahyudeny780@gmail.com>
2025-07-13 22:39:33 +0700

    server shutdown code

--- cmd/market-api/main.go
+++ cmd/market-api/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/fromcode/market-api/internal/config"
 )
@@ -25,11 +31,33 @@ func main() {
 		Handler: router,
 	}
 
+	slog.Info("Server mulai %s", slog.String("address", cfg.Addr))
 	fmt.Printf("Server mulai %s", cfg.HTTPServer.Addr) //Print pertanda dimulai server beserta portnya
 
-	err := server.ListenAndServe() //Listener pada untuk website
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe() //Listener pada website
+		if err != nil {
+			log.Fatal("Gagal mulai server")
+		}
+	}()
+
+	<-done
+
+	slog.Info("Matikan Server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	err := server.Shutdown(ctx)
 	if err != nil {
-		log.Fatal("Gagal mulai server")
+		slog.Error("Gagal mematikan server", slog.String("error", err.Error()))
 	}
+
+	slog.Info("Server berhasil dimatikan")
 
 }
